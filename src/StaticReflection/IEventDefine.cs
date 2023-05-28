037e27c5cf1657ba731a1b6b3a6ecd0641d77197
HCricle <none>
2023-05-28 20:43:13 +0800

    Add event transfer

--- src/StaticReflection/IEventDefine.cs
+++ src/StaticReflection/IEventDefine.cs
@@ -1,5 +1,43 @@
-﻿namespace StaticReflection
+﻿using System;
+
+namespace StaticReflection
 {
+    public class EventTransferEventArgs : EventArgs
+    {
+        public EventTransferEventArgs(IReadOnlyList<object> args)
+        {
+            Args = args;
+        }
+
+        public IReadOnlyList<object> Args { get; }
+    }
+    public abstract class EventTransfer : IEventTransfer
+    {
+        private readonly WeakEventManager<EventHandler<EventTransferEventArgs>> eventTransferWeakEventManager;
+
+        protected EventTransfer()
+        {
+            eventTransferWeakEventManager = new WeakEventManager<EventHandler<EventTransferEventArgs>>();
+        }
+
+        public event EventHandler<EventTransferEventArgs> EventTransfed
+        {
+            add
+            {
+                eventTransferWeakEventManager.Subscribe(value);
+            }
+
+            remove
+            {
+                eventTransferWeakEventManager.UnSubscribe(value);
+            }
+        }
+    }
+
+    public interface IEventTransfer
+    {
+        event EventHandler<EventTransferEventArgs> EventTransfed;
+    }
     public interface IEventDefine : IMemberDefine
     {
         Type DelegateType { get; }
---
+++ src/StaticReflection/WeakEventManager.cs
@@ -0,0 +1,34 @@
+﻿namespace StaticReflection
+{
+    public class WeakEventManager<TEventHandler>
+        where TEventHandler:Delegate
+    {
+        private readonly IDictionary<TEventHandler, WeakReference> handlers;
+
+        public WeakEventManager(IDictionary<TEventHandler, WeakReference> handlers)
+        {
+            this.handlers = handlers;
+        }
+
+        public WeakEventManager()
+            :this(new  Dictionary<TEventHandler, WeakReference>())
+        {
+        }
+
+        public IEnumerable<TEventHandler> Handlers => handlers.Keys;
+
+        public void Subscribe(TEventHandler handler)
+        {
+            var weakReference = new WeakReference(handler.Target, trackResurrection: false);
+            handlers[handler] = weakReference;
+        }
+        public bool UnSubscribe(TEventHandler handler)
+        {
+            return handlers.Remove(handler);
+        }
+        public void Clear()
+        {
+            handlers.Clear();
+        }
+    }
+}
